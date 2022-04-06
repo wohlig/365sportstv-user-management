f28fdbc5a3c2ea7366ba9864cdc220f975ad0688
samayyy <samaykgada09@gmail.com>
2022-04-07 00:16:43 +0530

    changes

--- mongooseModel/OtpCount.js
+++ mongooseModel/OtpCount.js
@@ -1,7 +1,6 @@
 var schema = new Schema(
     {
         mobile: { type: Number },
-        otp: { type: String, required: true },
         otpDate: { type: String },
         otpCount: { type: Number, default: 0 }
     },
