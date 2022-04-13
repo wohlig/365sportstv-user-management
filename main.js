3a24c061fcd8792365a3513e7e31a1d7fa85964c
samayyy <samaykgada09@gmail.com>
2022-04-13 16:18:18 +0530

    changes

--- main.js
+++ main.js
@@ -9,9 +9,21 @@ require(pwd + "/config/middleware.js")
 require("./lib/responses.js")
 require("./lib/controllers.js")
 require("./lib/models.js")
-require("./lib/views.js")
-
-app.listen(env["PORT"], () => {
+const server = app.listen(env["PORT"], () => {
     console.log(`Server Started at Port ${env["PORT"]}`)
     require(pwd + "/config/cron")
+})
+var io = require("socket.io")(server, {
+    cors: {
+        origins: ["http://localhost:8080"]
+    }
+})
+io.on("connection", (socket) => {
+    console.log("a user connected")
+    socket.on("disconnect", () => {
+        console.log("user disconnected")
+    })
+    socket.on("event", (id) => {
+        console.log(id)
+    })
 })
